37401f5ae0ceac4be5f375bb5b11d355940dec7b
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-05-13 22:32:42 +0600

    Delete

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const users = useLoaderData();
-    console.log(users);
+    const [allUsers, setAllUsers] = useState(users)
+    console.log(allUsers);
+    const handleDelete = _id =>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('Deleted successfully');
+                const remaining = allUsers.filter(user => user._id !== _id);
+                console.log(remaining);
+                setAllUsers(remaining);
+            }
+        })
+    }
     return (
         <div>
-            <h1>User: {users.length}</h1>
+            <h1 className='py-5'>User: {allUsers.length}</h1>
             {
-                users.map(user => <p key={user._id}>{user.email} <button>X</button> </p>)
+                allUsers.map(user => <p className='py-3 border mt-2 px-3' key={user._id}>{user.email} <button onClick= {() => handleDelete(user._id)} className='ml-4'>X</button> </p>)
             }
         </div>
     );
